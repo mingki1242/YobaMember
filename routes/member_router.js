3a5123ec8759f6e4cc82f6ea76b359891e63f26d
박민기 <86062121+mingki1242@users.noreply.github.com>
2023-12-01 17:24:31 +0900

    fin

--- routes/member_router.js
+++ routes/member_router.js
@@ -6,6 +6,7 @@ router.get("/" , async (req,res) => {
      let obj= {};
      let result = await dao.getAllList();
      obj.members = result;
+
      res.render("memberList" , obj);
 })
 
@@ -15,9 +16,27 @@ router.get("/regist" , async (req,res) => {
 
 router.post("/regist" , async (req,res) => {
      let obj ={};
-     let result = dao.registMember(req.body.name , req.body.email,req.body.password,req.body.money,req.body.position);
+     let result = await dao.registMember(req.body.name , req.body.email,req.body.password,req.body.money,req.body.position);
      res.redirect("/");
 })
+
+router.get("/detail/:id" , async (req,res) => {
+     let obj = {}
+     let result = await dao.readCntPlus(req.params.id);
+     obj.member = await dao.findById(req.params.id);
+     res.render("detailMember",obj);
+});
+
+router.get("/delete/:id" ,async (req,res) => {
+     await dao.deleteById(req.params.id);
+     res.redirect("/");
+})
+
+router.post("/modify", async (req,res) => {
+     await dao.updateMember(req.body.id,req.body.name,req.body.email,req.body.password,req.body.money,req.body.position);
+     res.redirect("/");
+})
+
 
 module.exports = router;
 
--- services/dao.js
+++ services/dao.js
@@ -14,11 +14,42 @@ async function getAllList() {
 async function registMember(name ,email,password, money,position)
 {
     const sql = "Insert into yoba (name , email , password , money ,position, regdate) values(?,?,?,?,?, sysdate())";
-    const result = db.query(sql , [name , email , password,money,position]);
+    const result = await db.query(sql , [name , email , password,money,position]);
     return result;
 
+}
+
+async function findById(id)
+{
+    const sql = "select * from yoba where id = ?";
+    const member = await db.query(sql , [id]);
+    return member[0];
+}
+
+async function deleteById(id)
+{
+    const sql = "delete from yoba where id = ?";
+    const result = await db.query(sql , [id]);
+}
+
+async function updateMember(id ,name ,email,password, money,position)
+{
+    const sql = "update yoba set name=? , email = ? , password = ? , money = ? ,position = ? where id = ?";
+    const result = await db.query(sql , [name , email , password , money,position,id]);
+    return result;
+}
+
+async function readCntPlus(id)
+{
+    const sql = "update yoba set readcnt = readcnt+1 where id = ?";
+    const result = await db.query(sql , [id]);
+    return result
 }
 module.exports = {
     getAllList,
     registMember,
+    findById,
+    deleteById,
+    updateMember,
+    readCntPlus,
 }
